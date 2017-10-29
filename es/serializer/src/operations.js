8989540b766a797fb27e3c9914c952ad42b19147
Ruslan Salikhov <ruslan.salihov@gmail.com>
2017-10-29 20:24:51 +0500

    fix

--- es/serializer/src/operations.js
+++ es/serializer/src/operations.js
@@ -600,20 +600,20 @@ export var credit_request_operation_ = new Serializer("credit_request_operation_
     fee: asset,
     borrower: protocol_id_type("account"),
     loan_asset: asset,
+    loan_period: uint32,
+    loan_persent: uint16,
     loan_memo: string,
-    deposit_asset: asset,
-    loan_persent: uint32,
-    loan_period: uint32
+    deposit_asset: asset
 });
 
 export var credit_request_operation = new Serializer("credit_request_operation", {
     fee: asset,
     borrower: protocol_id_type("account"),
     loan_asset: asset,
+    loan_period: uint32,
+    loan_persent: uint16,
     loan_memo: string,
-    deposit_asset: asset,
-    loan_persent: string,
-    loan_period: uint32
+    deposit_asset: asset
 });
 
 operation.st_operations = [transfer, limit_order_create, limit_order_cancel, call_order_update, fill_order, account_create, account_update, account_whitelist, account_upgrade, account_transfer, asset_create, asset_update, asset_update_bitasset, asset_update_feed_producers, asset_issue, asset_reserve, asset_fund_fee_pool, asset_settle, asset_global_settle, asset_publish_feed, witness_create, witness_update, proposal_create, proposal_update, proposal_delete, withdraw_permission_create, withdraw_permission_update, withdraw_permission_claim, withdraw_permission_delete, committee_member_create, committee_member_update, committee_member_update_global_parameters, vesting_balance_create, vesting_balance_withdraw, worker_create, custom, assert, balance_claim, override_transfer, transfer_to_blind, blind_transfer, transfer_from_blind, asset_settle_cancel, asset_claim_fees, credit_request_operation_, credit_request_operation];
